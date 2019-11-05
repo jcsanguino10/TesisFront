083bd9868ed52e78712cb830f3dbb082f2220843
Juan Camilo Sanguino <jc.sanguino10@uniandes.edu.co>
2019-11-05 08:34:03 -0500

    cards-salas-pcs

--- src/componentes/sala.jsx
+++ src/componentes/sala.jsx
@@ -8,21 +8,37 @@ export default class Sala extends Component {
             sala: this.props.sala
         }
         this.cargarEquipos = this.cargarEquipos.bind(this);
+        this.filaEquipos = this.filaEquipos.bind(this);
     }
     cargarEquipos() {
         let resp = [];
         if (this.state.sala === "Turing") {
-            resp.push(
-                <div className="container-fluid">
-                    <Link type="buttom" className="btn" to={"/equipo/ip/" + "120.032.231.495"}>
-                        <i className="fas fa-desktop fa-3x"></i>
-                        <p className="ip my-0">120.032.231.495</p>
-                    </Link>
-                </div>
-            )
+            for (let index = 0; index < 7; index++) {
+                resp.push(
+                    <div className="container-fluid row justify-content-center">
+                        {this.filaEquipos(6)}
+                    </div>
+                )
+            }
+        }
+        else if (this.state.sala === "Waira1") {
+
         }
         else {
 
+        }
+        return resp;
+    }
+
+    filaEquipos(filas) {
+        let resp = []
+        for (let index = 0; index < filas; index++) {
+            resp.push(
+                <Link type="buttom" className="btn" to={"/equipo/ip/" + "120.032.231.495"}>
+                    <i className="fas fa-desktop fa-3x"></i>
+                    <p className="ip my-0">120.032.231.495</p>
+                </Link>
+            )
         }
         return resp;
     }
